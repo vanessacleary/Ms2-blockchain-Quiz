e7e15af860e326f3596a1e856922b6741d443985
vanessacleary <vanessacleary@yahoo.com>
2021-08-30 21:29:20 +0000

    added save high score to local storage functionality to  end.js

--- assets/script/end.js
+++ assets/script/end.js
@@ -4,13 +4,38 @@ const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 finalScore.innerText = mostRecentScore;
 
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH_SCORES = 5;
+
+finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
-saveScoreBtn.dispabled = !username.value;
+    saveScoreBtn.dispabled = !username.value;
 });
 
 
 saveHighScore = e => {
-    console.log("clicked the save button!");
-    e.preventDefault();
-}
+console.log("clicked the save button!");
+e.preventDefault();
+
+const score = {
+    score: Math.floor(Math.random() * 100),
+    score: mostRecentScore,
+    name: username.value
+};
+
+highScores.push(score);
+
+highScores.sort((a, b) => b.score - a.score)
+
+highScores.splice(5);
+
+
+localStorage.setItem('highScores')
+
+
+console.log('highScores', JSON.stringify(highScores));
+window.location.assign('/');
+
+};
