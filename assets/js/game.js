dc9cd0538456258d13c932d268cf0153275bd08a
vanessacleary <vanessacleary@yahoo.com>
2021-09-04 14:05:37 +0000

    Feat/Bug: added pre question loader, ca't add to js at this point as questions are not loading

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,6 +6,9 @@ const choices = Array.from(document.getElementsByClassName("choice-text"));
 const progressText = document.getElementById("progressText");
 const scoreText = document.getElementById('score');
 const progressBarFull = document.getElementById('progressBarFull');
+const loader = document.getElementById('loader');
+const loader = document.getElementById('game');
+
 
 // Let
 let currentQuestion = {};
