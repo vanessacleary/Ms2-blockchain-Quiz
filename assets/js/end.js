786f5862bbe9f762ba4b2c01e3376d6ca480fe7d
vanessacleary <vanessacleary@yahoo.com>
2021-09-13 20:32:38 +0000

    added styling to highscores <li>, modal not showing - working on trouble shooting, time and home btn on game.html no aligning - working on troubleshooting

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,22 +1,23 @@
+//Constants
 const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 finalScore.innerText = mostRecentScore;
-
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
+//Number of highscores to be saved and shown on highscores page 
 const MAX_HIGH_SCORES = 5;
 
 finalScore.innerText = mostRecentScore;
 
+//Disables the save score button if no name put in
 username.addEventListener('keyup', () => {
     saveScoreBtn.disabled = !username.value;
 });
 
 
-saveHighScore = e => {
-    console.log("clicked the save button!");
+saveHighScore = (e) => {
     e.preventDefault();
 
     const score = {
@@ -25,17 +26,13 @@ saveHighScore = e => {
         name: username.value
     };
 
+    //Push highscore into an array
     highScores.push(score);
-
+    //Sorts highscores into hierarchical order
     highScores.sort((a, b) => b.score - a.score)
-
+    //Cuts the array of highscores so the top 5 appear
     highScores.splice(5);
 
-
-    localStorage.setItem('highScores')
-
-
-    console.log('highScores', JSON.stringify(highScores));
-    window.location.assign('/');
-
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('index.html');
 };
--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,12 +1,11 @@
+//Constants
 const highScoresList = document.getElementById("highScoresList");
 //Pulls array of highscores stored in local storage
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
 highScoresList.innerHTML = highScores
-//Displays highest scores from the array using the map method
+  //Displays highest scores from the array using the map method
   .map(score => {
     return `<li class="high-score">${score.name} - ${score.score}</li>`;
   })
   .join("");
-
-
