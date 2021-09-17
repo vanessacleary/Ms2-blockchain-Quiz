0c452be4776bb58b7f59a51c9c1b0ce0409c44af
vanessacleary <vanessacleary@yahoo.com>
2021-09-17 14:07:29 +0000

    Error found: changed innerTEXT to innerText on end.js

--- assets/js/end.js
+++ assets/js/end.js
@@ -4,32 +4,12 @@ const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 
-// //Displays different message depending on users score
-// const goodEffort = document.getElementById("good-effort");
-// const congrats = document.getElementById("congrats");
-// const noGood = document.getElementById("no-good");
-// const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
 const MAX_HIGH_SCORES = 5;
-
-let highScoresArray = [];
-highScores.forEach(function (obj) {
-  highScoresArray.push(obj.score);
-});
-
-// // compare the most rencent score with the scores in the highScoreArray
-// if (mostRecentScore > Math.max(...highScoresArray)) {
-//     // displays congrats message for a new high sore
-//     congrats.classList.remove("hidden");
-//   } else if (mostRecentScore == 0) {
-//     noGood.classList.remove("hidden");
-//   } else {
-//     // display a good effort message if it's not a new high score
-//     goodEffort.classList.remove("hidden");
-//   }
   
 
-finalScore.innerTEXT = mostRecentScore; //Not working
+finalScore.innerText = mostRecentScore; //Not working
 
 //Disables the save score button if no name put in
 username.addEventListener('keyup', () => {
